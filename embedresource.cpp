7a717c9eb2193cdcf101766eb931ccc2a91fdd21
person93 <person93.person93@gmail.com>
2021-05-11 10:06:30 -0400

    refactor: use std instead of boost

--- embedresource.cpp
+++ embedresource.cpp
@@ -1,50 +1,48 @@
-#include <boost/filesystem.hpp>
-#include <boost/filesystem/fstream.hpp>
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <string>
 
-using namespace std;
-using namespace boost::filesystem;
 
-int main(int argc, char** argv)
-{
-    if (argc < 3) {
-        fprintf(stderr, "USAGE: %s {sym} {rsrc}\n\n"
-                        "  Creates {sym}.c from the contents of {rsrc}\n",
-                argv[0]);
+int main( int argc, char** argv ) {
+    if ( argc < 3 ) {
+        std::cerr << "USAGE: " << *argv
+                  << " {sym} {rsrc}\n\n"
+                     "  Creates {sym}.c from the contents of {rsrc}\n";
         return EXIT_FAILURE;
     }
 
-    path dst{argv[1]};
-    path src{argv[2]};
+    std::filesystem::path dst{ argv[ 1 ] };
+    std::filesystem::path src{ argv[ 2 ] };
 
-    string sym = src.filename().string();
-    replace(sym.begin(), sym.end(), '.', '_');
-    replace(sym.begin(), sym.end(), '-', '_');
+    std::string sym = src.filename().string();
+    replace( sym.begin(), sym.end(), '.', '_' );
+    replace( sym.begin(), sym.end(), '-', '_' );
 
-    create_directories(dst.parent_path());
+    create_directories( dst.parent_path() );
 
-    boost::filesystem::ofstream ofs{dst};
+    std::ofstream ofs{ dst };
+    std::ifstream ifs{ src };
 
-    boost::filesystem::ifstream ifs{src};
-
-    ofs << "#include <stdlib.h>" << endl;
-    ofs << "const char _resource_" << sym << "[] = {" << endl;
+    ofs << "#include <stdlib.h>\n";
+    ofs << "const char _resource_" << sym << "[] = {\n";
 
     size_t lineCount = 0;
-    while (!ifs.eof())
-    {
+    while ( !ifs.eof() ) {
         char c;
-        ifs.get(c);
-        ofs << "0x" << hex << (c&0xff) << ", ";
-        if (++lineCount == 10) {
-            ofs << endl;
+        ifs.get( c );
+        ofs << "0x" << std::hex << ( c & 0xff ) << ", ";
+        if ( ++lineCount == 10 ) {
+            ofs << std::endl;
             lineCount = 0;
         }
     }
 
 
-    ofs << "};" << endl;
-    ofs << "const size_t _resource_" << sym << "_len = sizeof(_resource_" << sym << ");";
+    ofs << "};" << std::endl;
+    ofs << "const size_t _resource_" << sym << "_len = sizeof(_resource_" << sym
+        << ");";
 
     return EXIT_SUCCESS;
 }
