5195427b65700ccb99d576a37655c1e3f66d6cd9
person93 <person93.person93@gmail.com>
2021-05-11 13:40:30 -0400

    refactor: move Resource class into namespace

--- include/person93/resource.h
+++ include/person93/resource.h
@@ -4,24 +4,28 @@
 #include <cstddef>
 
 
-class Resource {
-public:
-    Resource( const std::byte* start, const std::size_t len )
-        : resource_data{ start }, data_len{ len } {}
+namespace person93::resource {
+    class Resource {
+    public:
+        Resource( const std::byte* start, const std::size_t len )
+            : resource_data{ start }, data_len{ len } {}
 
-    const std::byte* data() const { return resource_data; }
-    std::size_t size() const { return data_len; }
+        const std::byte* data() const { return resource_data; }
+        std::size_t size() const { return data_len; }
 
-    const char* begin() const { return resource_data; }
-    const char* end() const { return resource_data + data_len; }
+        const char* begin() const { return resource_data; }
+        const char* end() const { return resource_data + data_len; }
 
-private:
-    const std::byte* resource_data;
-    std::size_t data_len;
-};
+    private:
+        const std::byte* resource_data;
+        std::size_t data_len;
+    };
+}// namespace person93::resource
+
 #define LOAD_RESOURCE( RESOURCE )                                                      \
     ( []() {                                                                           \
         extern const std::byte* const _resource_##RESOURCE;                            \
         extern const std::size_t _resource_##RESOURCE##_len;                           \
-        return Resource( _resource_##RESOURCE, _resource_##RESOURCE##_len );           \
+        return ::person93::resource::Resource( _resource_##RESOURCE,                   \
+                                               _resource_##RESOURCE##_len );           \
     } )()
