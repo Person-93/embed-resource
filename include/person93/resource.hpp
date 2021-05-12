e8ee4e863fb1fa05bd01e8cc4a7be82e835e50c5
person93 <person93.person93@gmail.com>
2021-05-11 21:05:59 -0400

    refactor: use std::string_view instead of Resource class

--- include/person93/resource.hpp
+++ include/person93/resource.hpp
@@ -1,31 +1,12 @@
 #pragma once
 
-#include <array>
-#include <cstddef>
+#include <string_view>
 
 
-namespace person93::resource {
-    class Resource {
-    public:
-        Resource( const std::byte* start, const std::size_t len )
-            : resource_data{ start }, data_len{ len } {}
-
-        const std::byte* data() const { return resource_data; }
-        std::size_t size() const { return data_len; }
-
-        const char* begin() const { return resource_data; }
-        const char* end() const { return resource_data + data_len; }
-
-    private:
-        const std::byte* resource_data;
-        std::size_t data_len;
-    };
-}// namespace person93::resource
-
-#define LOAD_RESOURCE( RESOURCE )                                                      \
-    ( []() {                                                                           \
-        extern const std::byte* const _resource_##RESOURCE;                            \
-        extern const std::size_t _resource_##RESOURCE##_len;                           \
-        return ::person93::resource::Resource( _resource_##RESOURCE,                   \
-                                               _resource_##RESOURCE##_len );           \
-    } )()
+#define PERSON93_RESOURCE( RESOURCE )                                                  \
+    []() {                                                                             \
+        extern const char _resource_##RESOURCE[];                                      \
+        extern const ::std::size_t _resource_##RESOURCE##_len;                         \
+        return ::std::string_view{ _resource_##RESOURCE,                               \
+                                   _resource_##RESOURCE##_len - 1 };                   \
+    }()
