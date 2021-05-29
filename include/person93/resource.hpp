3005053d24531cea0aa94e780d393c9eb711520c
person93 <person93.person93@gmail.com>
2021-05-29 10:14:21 -0400

    fix: silence warnings from resource macro

--- include/person93/resource.hpp
+++ include/person93/resource.hpp
@@ -2,11 +2,17 @@
 
 #include <string_view>
 
-
+#ifndef __clang_analyzer__
 #define PERSON93_RESOURCE( RESOURCE )                                                  \
     []() {                                                                             \
         extern const char _resource_##RESOURCE##_data[];                               \
         extern const ::std::size_t _resource_##RESOURCE##_len;                         \
-        return ::std::string_view{ _resource_##RESOURCE##_data,                        \
-                                   _resource_##RESOURCE##_len - 1 };                   \
+        return ::std::string_view{                                                     \
+                static_cast< const char* >( _resource_##RESOURCE##_data ),             \
+                _resource_##RESOURCE##_len - 1 };                                      \
     }()
+#else
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define PERSON93_RESOURCE( RESOURCE )                                                  \
+    ::std::string_view {}
+#endif
